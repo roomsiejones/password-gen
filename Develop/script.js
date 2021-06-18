7a5d2e2e3ac608cf51f33d2440476699c4fc30ff
Roomsie Jones <seangravito@gmail.com>
2021-06-18 14:05:17 -0500

    made function to get user inputs

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowLetters = "abcdefghijklmnopqrstuvwxyz";
+var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ var upCase = confirm("Do you wish to include uppercase letters? (Click 'Cancel' for no)");
+ var lowCase = confirm("Do you wish to include lowercase letters?");
+ var numSel = confirm("Do you wish to include numbers?");
+ var symbol = confirm("Do you wish to include symbols?");
+ var lenSel = prompt("Please type desired password length.". "8-128");
 
-  passwordText.value = password;
+ for i=0; i = length; i++ (
+   if i=length{
+     return;
+   } else {
+        if (upCase){
+          upper()
+        };
+
+
+   }
+ )
+
+
+ 
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
